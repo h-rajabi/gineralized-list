732ac166a1f1a875131e7cecf30a44b548b89b9a
hossein <s.h.r.rajabi2000@gmail.com>
2024-11-26 03:56:25 +0330

    finish create list

--- generalized-lists/main.cpp
+++ generalized-lists/main.cpp
@@ -10,7 +10,7 @@ class node
 {
     private:
         char variable;
-        float coef;
+        int coef;
         int exp,tag;
         node *link;
         node *dlink;
@@ -36,11 +36,11 @@ class node
         }
         void setVariable(char v){ this->variable=v; }
         void setCoef(float c){ this->coef=c; }
-        void setEcp(int e){ this->exp=e; }
+        void setExp(int e){ this->exp=e; }
         void setLink(node *l){ this->link=l; }
         void setDlink(node *dl){ this->dlink=dl; }
         char getVariable(){return this->variable;}
-        float getCoef(){return this->coef;}
+        int getCoef(){return this->coef;}
         int getExp(){return this->exp;}
         int getTag(){return this->tag;}
         node* getLink(){return this->link;}
@@ -69,53 +69,54 @@ class generalizedList
         void add_head(char v);
         void add_poly(int e);
         void add_h(float c,int e);
-        void par_print_list();
         void print_list(){
             sub_print_list(this->first);
         };
 
 };
 
-void read_from_file();
+node* read_from_file();
 void sort_array();
 bool compare_row(const vector<int>& row1, const vector<int>& row2){
     return row1.back() > row2.back();
 };
-
-
+void create_list(vector<vector<int>>& a,vector<char>& v,node* head,int r,int c);
+node* find_current_node(node* first,int e);
+node* create_current_row(node* first, int e);
+void create_current_row3(node* first,int c,int e);
 int main()
 {
-    // read_from_file();
-    // generalizedList testl;
-    // node *x=new node(4,2);
-    // node *y=new node(5,1);
-    // node *z=new node(8,0);
+    generalizedList* obj1=new generalizedList();
+    obj1->setFirst(read_from_file());
+    cout<<"-----------------------\n";
+    obj1->print_list();
+    // node* head=obj1->getFirst();
+    // cout<<head->getVariable()<<"^"<<head->getLink()->getExp()<<"(";
+    // head=head->getLink()->getDlink();
+    // cout<<head->getVariable()<<"^"<<head->getLink()->getExp()<<"(";
+    // head=head->getLink()->getDlink();
+    // cout<<head->getVariable()<<"^"<<head->getLink()->getExp()<<"("<<head->getLink()->getCoef()<<")";
 
-    // testl.add_head('x');
-    // node *f= testl.getFirst();
-    // f->setLink(x);
-    // x->setLink(y);
-    // y->setLink(z);
-    // testl.print_list();
+    cout<<endl;
     return 0;
 }
 
 void generalizedList::sub_print_list(node* head){
     
     char v = head->getVariable();
-    while (head)
-    {
-        if(head->getTag() == 2){
-            cout<<"(";
+    while (head) {
+        if (head->getTag() == 2) {
+            cout << "(";
+            if (head->getDlink())
+            {
             sub_print_list(head->getDlink());
-            cout << ")"<<v<<"^"<<head->getExp()<<"+";
+            }else cout<<"error here";
+            cout << ")"<<v << "^" << head->getExp();
+        } else if (head->getTag() == 3) {
+            cout << "+" << head->getCoef() << v<<"^" << head->getExp();
         }
-        else if (head->getTag() != 1){
-            cout<<"+"<<head->getCoef()<<v<<"^"<<head->getExp();
-        }
-        head=head->getLink();
-    }
-    
+        head = head->getLink();
+    }   
 }
 
 void generalizedList::clear_list(node* head){
@@ -139,13 +140,12 @@ void generalizedList::add_head(char v){
     }
 }
 
-void read_from_file(){
+node* read_from_file(){
     ifstream InFile("in.txt");
     if(!InFile) {cout<<"error! :can not open file partner\n";}
     int numC,numR;
     InFile >> numC >> numR;
     vector<vector<int>> farray(numR,vector<int>(numC+1));
-    vector<char> variable={'x','y','z','i','e','f','g','h'};
     for (int i = 0; i < numR; i++)
     {
         for (int j = 0; j < numC+1; j++)
@@ -154,31 +154,195 @@ void read_from_file(){
         }
     }
     
-    InFile.close();
-
-    for (int i = 0; i < numR; i++)
-    {
-        for (int j = 0; j < numC+1; j++)
-        {
-            cout<<farray[i][j]<<"\t";
-        }
-        cout<<endl;
-    }
-    cout<<"--------------*-------------"<<endl;
-    
+    InFile.close();    
     sort(farray.begin(),farray.end(),compare_row);
-    
-    for (int i = 0; i < numR; i++)
-    {
-        for (int j = 0; j < numC+1; j++)
-        {
-            cout<<farray[i][j]<<"\t";
-        }
-        cout<<endl;
-    }
-
-
-
+    vector<char> varray={'x','y','z','i','e','f','g','h'};
+    node *head=new node(varray[numC-1]);
+    create_list(farray,varray,head,numR,numC);
+    return head;
 }
 
+/*
+void create_list(vector<vector<int>>& a,vector<char>& v,node* head,int r,int c){
+    node *current=head;
+    node *tail=nullptr;
+    for (int i = 0; i < r; i++)
+    {
+        if (current->getTag() == 1)
+        {
+            current->setLink(new node(a[i][c]));
+            current=current->getLink();
+        }else if(current->getExp() != a[i][c])
+        {
+            current->setLink(new node(a[i][c]));
+            current=current->getLink();
+        }
+        
+        node *temp=nullptr;
+        for (int j = c-1; j >= 1; j--)
+        {   
 
+            if (current->getDlink())
+            {
+                temp=current->getDlink();
+            }else
+            {
+                temp = new node(v[j-1]);
+                current->setDlink(temp);
+            }
+            
+            if (temp->getLink())
+            {   
+                temp = temp->getLink();
+                if (j>1)
+                {   
+                    if (temp->getExp() != a[i][j])
+                    {
+                        temp->setLink(new node(a[i][j]));  
+                    }
+                }else
+                {   
+                    if (temp->getDlink())
+                    {
+                        temp=temp->getDlink();
+                        node* tail=temp->getLink();
+                        while (tail->getLink())
+                        {
+                            if (tail->getExp() == a[i][j])
+                            {   
+                                tail->setCoef(tail->getCoef()+a[i][j-1]);
+                                break;
+                            }
+                            else
+                            {
+                                tail=tail->getLink();
+                            }
+                        }
+                        if (!tail->getLink())
+                        {
+                            tail->setLink(new node(a[i][j-1],a[i][j]));
+                        }
+                        
+                    }else
+                    {
+                        temp->setDlink(new node(v[j-1]));
+                        temp=temp->getDlink();
+                        temp->setLink(new node(a[i][j-1],a[i][j]));
+                    }
+                }
+            }else
+            {
+                if (j>1)
+                {
+                    temp->setLink(new node(a[i][j]));
+                    temp=temp->getLink();   
+                }else
+                {   temp->setDlink(new node(v[j-1]));
+                    temp=temp->getDlink();
+                    temp->setLink(new node(a[i][j-1],a[i][j]));
+                }
+            }
+        }
+    }
+}
+*/
+
+node* find_current_node(node* first,int e){
+    if (first->getExp()== e)
+    {
+        return first;
+    }
+    
+    while (first->getLink())
+    {
+        if (first->getExp() == e)
+        {   
+            // cout<<"find:" <<first<<endl;
+
+            return first;
+        }
+        first = first->getLink();
+    }
+
+    first->setLink(new node(e));
+    // cout<<"not find:"<<first<<endl;
+    return first->getLink();
+}
+node* create_current_row(node* first, int e){
+    if (first->getLink())
+    {   
+        // cout<<find_current_node(first->getLink(),e);
+        return find_current_node(first->getLink(),e);
+
+    }else
+    {   
+        first->setLink(new node(e));
+        // cout<<"first is :"<<first->getVariable()<<"ad :"<<first->getLink()<<endl;
+        return first->getLink();
+    }
+}
+void create_current_row3(node* first,int c,int e){
+    if (first->getLink())
+    {   
+        first=first->getLink();
+        while (first->getLink())
+        {
+            if (first->getExp() == e)
+            {   
+                first->setCoef(first->getCoef()+c);
+                return ;
+            }
+            first=first->getLink();                        
+        }
+        if (!first->getLink())
+        {
+            first->setLink(new node(c,e));
+        }
+    }else
+    {
+        first->setLink(new node(c,e));
+    }
+}
+
+void create_list(vector<vector<int>>& a,vector<char>& v,node* head,int r,int c){
+    node* temp=nullptr;
+    node* tail=nullptr;
+    // cout<<"ad head :"<<head<<endl;
+    for (int i = 0; i < r; i++)
+    {
+        tail=head;
+        for (int j = c; j >= 1; j--)
+        {
+            if (j>1)
+            {
+                if (tail->getVariable() == v[j-1])
+                {   
+                    
+                    temp = create_current_row(tail,a[i][j]);
+                }else
+                {
+                    if (temp->getDlink())
+                    {
+                        tail=temp->getDlink();
+                    }else
+                    {
+                        temp->setDlink(new node(v[j-1]));
+                        tail=temp->getDlink();
+                    }
+                    temp=create_current_row(tail,a[i][j]);
+                }
+            }else
+            {
+                if (temp->getDlink())
+                {
+                    tail=temp->getDlink();
+                }else
+                {
+                    temp->setDlink(new node(v[j-1]));
+                    tail=temp->getDlink();
+                }
+                create_current_row3(tail,a[i][j-1],a[i][j]);                
+            }
+        }
+    }
+}
